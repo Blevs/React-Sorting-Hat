01f9afc5d6d17794ce5fe199df8e6b9c0f1bd3d7
Henry Blevins <heblevi@gmail.com>
2019-03-21 12:56:30 -0400

    Implement proof of concept questions

--- react-sorting-hat/src/scenes/Questions/index.js
+++ react-sorting-hat/src/scenes/Questions/index.js
@@ -1,8 +1,21 @@
 import React from "react";
+import questions from "./questions.js";
 
 const Questions = (props) => {
     return (
-            <div>Questions</div>
+        <div>
+          <h1> Questions </h1>
+          <div>
+            {questions.map(q => {
+                return (
+                    <div>
+                      <h2>{q.question}</h2>
+                      {q.answers.map(a => <div points={a.points}>{a.text}</div>)}
+                    </div>
+                );
+            })}
+          </div>
+        </div>
     );
 };
 
---
+++ react-sorting-hat/src/scenes/Questions/questions.js
@@ -0,0 +1,46 @@
+const questions = [
+    {
+        question: "How do you eat a sandwich?",
+        answers: [
+            {
+                text: "Bravely",
+                points: {gryfindor: 1}
+            },
+            {
+                text: "Evilly",
+                points: {slytherin: 1}
+            },
+            {
+                text: "Smartly",
+                points: {ravenclaw: 1}
+            },
+            {
+                text: "Fast",
+                points: {hufflepuff: 1}
+            }
+        ]
+    },
+    {
+        question: "Which position in Quidditch would you want to play?",
+        answers: [
+            {
+                text: "Keeper",
+                points: {gryfindor: 1}
+            },
+            {
+                text: "Beater",
+                points: {slytherin: 1}
+            },
+            {
+                text: "Seeker",
+                points: {ravenclaw: 1}
+            },
+            {
+                text: "Chaser",
+                points: {hufflepuff: 1}
+            }
+        ]
+    },
+];
+
+export default questions;
