24231e77d0d8ed974c3e6b090b65c4c1e609f57a
Henry Blevins <heblevi@gmail.com>
2019-03-21 18:00:46 -0400

    Add scss, and begin styling

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "./scenes/Home/";
 import Quiz from "./scenes/Quiz/";
--- react-sorting-hat/src/scenes/Home/index.js
+++ react-sorting-hat/src/scenes/Home/index.js
@@ -3,8 +3,21 @@ import { Link } from "react-router-dom";
 
 const Home = (props) => {
     return (
-        <div>
-          Home
+        <div className="home">
+          <h1>Hogwarts School of Witchcraft and Wizardry</h1>
+          <h2 className="byline">Dear .............,</h2>
+          <div className="body">
+            <p>
+              We are pleased to inform you that you have been accepted at Hogwarts School of Witchcraft and Wizardry.
+            </p>
+            <p>
+              Students shall be requied to report to the Chamber of Reception upon arrival, the dates for which shall be duly advised.
+            </p>
+            <p>
+              Due to resurfaced tweets, the Sorting Hat has been let go. House selection will now be done via a Highly-Advanced Machine Learning Algorithm, which is from the Magical School of Linear Algebra.
+            </p>
+            <Link to="/quiz" />
+          </div>
           <Link to="/quiz">Quiz</Link>
         </div>
     );
